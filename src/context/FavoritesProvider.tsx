c599ec52e153c7bd520c76537f3582e27ebc0fbe
Carlos Verdugo <carlos.versa99@gmail.com>
2026-09-09 11:11:25 -0700

    Add use effect instea for favorite provider

--- src/context/FavoritesProvider.tsx
+++ src/context/FavoritesProvider.tsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FavoritesContext } from './FavoritesContext';
 
 export const FavoritesProvider = ({ children }: { children: React.ReactNode }) => {
 
     const [favorites, setFavorites] = useState<string[]>(JSON.parse(localStorage.getItem("favorites") || "[]"));
 
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    }, [favorites]);
+
     const addFavorite = (id: string) => {
         setFavorites((prevFavorites) => [...prevFavorites, id]);
-        localStorage.setItem("favorites", JSON.stringify([...favorites, id]));
+        // localStorage.setItem("favorites", JSON.stringify([...favorites, id]));
     }
 
     const removeFavorite = (id: string) => {
         setFavorites((prevFavorites) => prevFavorites.filter((favId) => favId !== id));
-        localStorage.setItem("favorites", JSON.stringify(favorites.filter((favId) => favId !== id)));
+        // localStorage.setItem("favorites", JSON.stringify(favorites.filter((favId) => favId !== id)));
     }
 
     const isFavorite = (id: string) => {
